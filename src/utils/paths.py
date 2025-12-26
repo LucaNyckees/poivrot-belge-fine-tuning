a9c981bb80a5da155279bc5a0c11c41cf1169f2d
LucaNyckees <nyckees.luca@gmail.com>
2025-12-26 14:43:32 +0000

    minor

--- src/utils/paths.py
+++ src/utils/paths.py
@@ -3,8 +3,10 @@ from pathlib import Path
 
 
 BASE_PATH = Path(os.getenv("BASE_PATH", Path(__file__).parents[2]))
-print("BASE_PATH:", BASE_PATH)
 TRAINING_DATA_PATH = BASE_PATH / "scripts"
 RESULTS_PATH = Path(os.getenv("RESULTS_PATH", BASE_PATH / "results"))
 MODELS_PATH = Path(os.getenv("MODELS_PATH", BASE_PATH / "poivrot_belge_lora"))
 SRC_PATH = Path(os.getenv("SRC_PATH", BASE_PATH / "src"))
+
+for path in [TRAINING_DATA_PATH, RESULTS_PATH, MODELS_PATH]:
+    os.makedirs(path, exist_ok=True)
